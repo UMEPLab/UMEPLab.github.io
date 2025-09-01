e4e3ffae863ff8a8096ea767057381e16d2f75b5
februarysea <februarysea@februaryseaMac-mini.local>
2025-09-01 21:38:43 +0800

    add site configure

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
+  site: 'https://umep-lab.github.io/',   // 新增这一行
   integrations: [tailwind()]
 });
+
