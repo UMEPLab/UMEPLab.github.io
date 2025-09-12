224f94283a558b4510cc7a4e652368217b716ef5
februarysea <31513558+februarysea@users.noreply.github.com>
2025-09-12 20:53:35 +0800

    Fix formatting in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import tailwind from '@astrojs/tailwind';
 export default defineConfig({
   site: 'https://umep-lab.github.io/',
   integrations: [tailwind()],
-    output: 'static' 
+  output: 'static' 
 
 });
