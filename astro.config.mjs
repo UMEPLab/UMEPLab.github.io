54b80ed6d0d6c8b08ee617bcc7b84a34a1cca0c8
februarysea <februarysea@qq.com>
2025-09-12 20:50:25 +0800

    fix astro_config's bug

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   site: 'https://umep-lab.github.io/',
-  integrations: [tailwind()]
-});
+  integrations: [tailwind()],
+    output: 'static' 
 
+});
